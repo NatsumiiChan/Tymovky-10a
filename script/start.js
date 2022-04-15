e25dca3ad7642fc38b724fd2329ada13e3a09ebc
maranacze <marana2003cz@seznam.cz>
2022-04-15 15:10:58 +0200

    ....

--- script/start.js
+++
@@ -1,1 +0,0 @@
-document.querySelector(".zahlavi").innerHTML += "<img src='images/" + (Math.floor(Math.random() * 10) + 1) + ".jpg' alt='HOME' class='home_img'>";
--- script/start2.js
+++ script/start2.js
@@ -1,5 +1,24 @@
 function zpusobove_okno(){
-    document.querySelector("#zpusobovy_nositel").classList.toggle("okno_zpusobove")
+    if (document.querySelector("#zpusobovy_nositel").classList.contains("okno_zpusobove") == true){
+        document.querySelector("#zpusobovy_nositel").classList.add("okno_zaviraci");
+        setTimeout(() => {document.querySelector("#zpusobovy_nositel").classList.remove("okno_zpusobove");document.querySelector("#zpusobovy_nositel").classList.remove("okno_zaviraci"); 
+        }, 309);
+    }else{
+        document.querySelector("#zpusobovy_nositel").classList.toggle("okno_zpusobove");
+        zmenovac_velikosti();
+    }
 }
 
 document.querySelector(".zahlavi").innerHTML += "<img src='../images/" + (Math.floor(Math.random() * 10) + 1) + ".jpg' alt='HOME' class='home_img'>";
+
+window.onresize = zmenovac_velikosti;
+
+function zmenovac_velikosti(){
+    if (document.querySelector(".kontejner_zpusobovy").clientHeight > (window.innerHeight-50)){
+        document.querySelector(".okno_zpusobove").classList.add("okno_zpusobove_true");
+    }else{
+        if(document.querySelector("#zpusobovy_nositel").classList.contains("okno_zpusobove") == true){
+            document.querySelector(".okno_zpusobove").classList.remove("okno_zpusobove_true");
+        }
+    }
+}
