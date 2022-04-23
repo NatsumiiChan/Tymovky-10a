d712d546c6a10709190b61d491041432ed6d66eb
maranacze <marana2003cz@seznam.cz>
2022-04-23 18:15:02 +0200

    ...

--- script/popup.js
+++ script/popup.js
@@ -1,10 +1,12 @@
 function vysunSe(){
     if (document.querySelector("#interier_popupu").classList.contains("kontejner_vyberovy")  == true){
         document.querySelector("#interier_popupu").classList.add("kontejner_vyberovy_zaviraci");
+        document.querySelector("#zavirac_popupu").classList.add("nic");
         setTimeout(() => {document.querySelector("#interier_popupu").classList.remove("kontejner_vyberovy");document.querySelector("#interier_popupu").classList.remove("kontejner_vyberovy_zaviraci");document.querySelector("#nastavovani").classList.remove("nastaveni_true");
         }, 500);
     } else{
         document.querySelector("#interier_popupu").classList.add("kontejner_vyberovy");
         document.querySelector("#nastavovani").classList.add("nastaveni_true");
+        document.querySelector("#zavirac_popupu").classList.remove("nic");
     }
 }
