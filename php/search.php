20608b48e928b91a9efb9d476ba3df17efe60525
NatsumiiChan <natsumiminakochan@gmail.com>
2022-06-20 22:21:19 +0200

    UwU

--- php/search.php
+++ php/search.php
@@ -1,86 +1,14 @@
-// zatim nefunkcni, pouze testovaci verze
-
 <?php
     include "data.php";
-    $c1 = $_POST['color1'];
-    $c2 = $_POST['color2'];
-    $c3 = $_POST['color3'];
-    $search = "";
+    $searchIndex = $_POST['searchNewIndex'];
 
-    switch ($c1) {
-        case "":
-            $search += "E";
-            break;
-        case "red":
-            $search += "R";
-            break;
-        case "yellow":
-            $search += "Y";
-            break;
-        case "green":
-            $search += "G";
-            break;
-        case "white":
-            $search += "W";
-            break;
-        case "blue":
-            $search += "B";
-            break;
+    $connect = new mysqli($host, $user, $psw, $db);
+    $sql = "SELECT type, category, name, description FROM navestidla WHERE search_index = '$searchIndex'";
+    $result = mysqli_query($connect, $sql);
+    $row = mysqli_fetch_assoc($result);
+    if ($row) {
+        echo $row['type'] . "," . $row['category'] . "," . $row['name'] . "," . $row['description'];
+    } else {
+        echo "neni";
     }
-
-    switch ($c2) {
-        case "":
-            $search += "E";
-            break;
-        case "red":
-            $search += "R";
-            break;
-        case "yellow":
-            $search += "Y";
-            break;
-        case "green":
-            $search += "G";
-            break;
-        case "white":
-            $search += "W";
-            break;
-        case "blue":
-            $search += "B";
-            break;
-    }
-
-    switch ($c3) {
-        case "":
-            $search += "E";
-            break;
-        case "red":
-            $search += "R";
-            break;
-        case "yellow":
-            $search += "Y";
-            break;
-        case "green":
-            $search += "G";
-            break;
-        case "white":
-            $search += "W";
-            break;
-        case "blue":
-            $search += "B";
-            break;
-    }
-
-    $connect = new mysqli($host, $user, $psw, $db) or die("nepovedlo se pripojit");
-    echo "pripojeno<br>";
-
-    $connect->set_charset("UTF8") or die("nenastaveno");
-    echo "nastaveno<br>";
-
-    $SQL = "SELECT type, name, description, image_src, search_index FROM navestidla WHERE search_index='.$search.'";
-    $result = $connect->query($SQL) or die("nepovedlo se ".$SQL);
-    
-    echo $row->type." ".$row->name." ".$row->description."<br>";
-
-    $result->free();
-    $connect->close();
 ?>
--- script/modal.js
+++ script/modal.js
@@ -1,26 +1,28 @@
-function zpusobove_okno(){
-    if (document.querySelector("#zpusobovy_nositel").classList.contains("okno_zpusobove") == true){
-        document.querySelector("#zpusobovy_nositel").classList.add("okno_zaviraci");
-        setTimeout(() => {document.querySelector("#zpusobovy_nositel").classList.remove("okno_zpusobove");document.querySelector("#zpusobovy_nositel").classList.remove("okno_zaviraci");if(document.querySelector("#zpusobovy_nositel").classList.contains("okno_zpusobove_true") == true){
-            document.querySelector("#zpusobovy_nositel").classList.remove("okno_zpusobove_true");
+function zpusobove_okno(id){
+    id = "#" + id;
+    console.log(id);
+    if (document.querySelector(id).classList.contains("okno_zpusobove") == true){
+        document.querySelector(id).classList.add("okno_zaviraci");
+        setTimeout(() => {document.querySelector(id).classList.remove("okno_zpusobove");document.querySelector(id).classList.remove("okno_zaviraci");if(document.querySelector(id).classList.contains("okno_zpusobove_true") == true){
+            document.querySelector(id).classList.remove("okno_zpusobove_true");
         }
         }, 509);
         document.querySelector("#body").style.overflow = "overlay";
     }else{
-        document.querySelector("#zpusobovy_nositel").classList.toggle("okno_zpusobove");
-        zmenovac_velikosti();
+        document.querySelector(id).classList.toggle("okno_zpusobove");
+        zmenovac_velikosti(id);
         document.querySelector("#body").style.overflow = "hidden";
     }
 }
 
 window.onresize = zmenovac_velikosti;
 
-function zmenovac_velikosti(){
+function zmenovac_velikosti(id){
     if (document.querySelector(".kontejner_zpusobovy").clientHeight > (window.innerHeight-50)){
         document.querySelector(".okno_zpusobove").classList.add("okno_zpusobove_true");
     }else{
-        if(document.querySelector("#zpusobovy_nositel").classList.contains("okno_zpusobove_true") == true){
-            document.querySelector("#zpusobovy_nositel").classList.remove("okno_zpusobove_true");
+        if(document.querySelector(id).classList.contains("okno_zpusobove_true") == true){
+            document.querySelector(id).classList.remove("okno_zpusobove_true");
         }
     }
 }
